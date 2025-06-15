7ea3403597e4f4eb2e7c7224ce30824cf5dcd7b6
devestrator <bhaskarkumararya900@gmail.com>
2025-06-15 20:19:03 +0530

    first commit

---
+++ ToDoList/ToDolist.py
@@ -0,0 +1,75 @@
+
+import json
+from datetime import datetime
+
+file_name = "ToDoListData.json"
+class Task :  
+    """A class to represent a task."""
+    def __str__(self):
+        return f"id : {self.id} {self.description} [{self.completed}]"
+    def __repr__(self):
+        return f"Task(description={self.description}, completed={self.completed})"
+    def __init__(self,Description,id = -1): 
+        self.description = Description
+        self.completed = False
+        self.id = datetime.now().strftime("%Y%m%d%H%M%S%f") if id == -1 else str(id)
+    def toDictItem (self) :
+        return  {"description" : self.description , "completed" : f"{self.completed}"}
+
+    
+
+
+def loadData () -> dict :
+    try :
+        with open(file_name,"r") as file :
+            return json.load(file)
+    except FileNotFoundError :
+        with open(file_name) :
+            return {"tasks" : {}}
+        
+def saveData (data) :
+    with open(file_name,"w") as file :
+        json.dump(data,file,indent=4)
+
+def AddTask (description) :
+    data = loadData()
+    task = Task(description)
+    data["tasks"][task.id] = task.toDictItem()
+    saveData(data)
+
+def updateTaskDataFromId (id: int) :
+    tasks = loadData()["tasks"]
+    
+
+
+def viewTasks () :
+    print(loadData()["tasks"])
+        
+
+def markTaskAsComplete (id : str) :
+    data = loadData()
+    tasks = data["tasks"]
+    tasks[id]["completed"] = "True"
+    saveData(data)
+
+def main() :
+    markTaskAsComplete(str(20250615155349499090))
+    while True :    
+        response = input("""
+        1. view task
+        2. create task 
+        3. mark task as complete
+""").strip()
+        if response == "1" :
+            viewTasks()
+        elif response == "2" :
+            AddTask(input("describe the task : "))   
+        elif response == "3" :
+            task = input("Enter task to mark as complete: ")
+            markTaskAsComplete(task)    
+        else :
+            print("Invalid option, please try again.")
+    
+
+
+main()
---
+++ geminiChatBot/Geminibot.py
@@ -0,0 +1,75 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+
+client = genai.Client(api_key="")
+#chat = client.chats.create(model="gemini-2.0-flash-preview-image-generation")
+def drawImageTool (_input : str) -> list[str,BytesIO] :
+    o = ["",None]
+    response = client.models.generate_content(
+    model="gemini-2.0-flash-preview-image-generation",
+    contents=_input,
+    config=types.GenerateContentConfig(
+      response_modalities=['TEXT', 'IMAGE']
+    )
+)
+
+    for part in response.candidates[0].content.parts:
+        if part.text is not None:
+            o[0] += part.text 
+            print(f"response from VLM : {part.text}")
+        elif part.inline_data is not None:
+            o[1] = BytesIO(part.inline_data.data)
+            image = Image.open(BytesIO((part.inline_data.data)))
+            image.save('gemini-native-image.png')
+            image.show()
+    return o        
+
+CreateImageFunc = {
+    "name": "CreateImage",
+    "description": "connects and sends a message to an API , which might return an image from the promt , and also include some text response",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "prompt": {
+                "type": "string",
+                "description": "the prompt for the model to draw the image",
+            },
+        },
+        "required": ["prompt"],
+    },
+}
+
+tools = tools = types.Tool(function_declarations=[CreateImageFunc])
+config = types.GenerateContentConfig(tools=[tools])
+contents = []
+while True :
+    contents.append(types.Content(parts=[types.Part(text=input("your turn : "))],role= "user"))
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents= contents,
+    config=config,
+)
+    if response.candidates[0].content.parts[0].function_call:
+        function_call = response.candidates[0].content.parts[0].function_call
+        print(f"Function to call: {function_call.name}")
+        print(f"Arguments: {function_call.args}")
+        if function_call.name == "CreateImage" :
+            r = drawImageTool(function_call.args["prompt"])
+            function_response_part = types.Part.from_function_response(
+            name=function_call.name,
+            response={"result": "response from the model : " + r[0] + "received an image " if r[1] != None else "no image received" },
+            )
+            contents.append(response.candidates[0].content) # Append the content from the model's response.
+            contents.append(types.Content(role="user", parts=[function_response_part])) # Append the function response
+            final_response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            config=config,
+            contents= contents,
+            )
+            print(final_response.text)
+
+    else:
+        print("No function call found in the response.")
+        print(response.text)
