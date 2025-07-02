a27c94db9de9738d63cf253780c59c12a32a54be
devestrator <bhaskarkumararya900@gmail.com>
2025-07-02 20:34:38 +0530

    added profile, and follow in social media app, and solved lots of backtracking questions

--- Social_media_app/core/urls.py
+++ Social_media_app/core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('signin',views.signin,name='signin'),
     path('logout',views.logout,name='logout'),
     path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like_post'),
+    path('profile/<str:pk>',views.profile,name="profile") ,
+    path('follow',views.follow,name='follow'),
 ]
--- Social_media_app/core/views.py
+++ Social_media_app/core/views.py
@@ -3,13 +3,32 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import *
 
 # Create your views here.
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'index.html',{'user_profile' : user_profile,'posts' : posts})
+
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST' :
+        follower = request.POST.get('follower') 
+        user = request.POST.get('user')
+        if followercount := FollowersCount.objects.filter(follower=follower,user=user).first():
+            followercount.delete()
+            return redirect('/profile/' + user)
+        else :
+            new_follower = FollowersCount.objects.create(follower=follower,user=user)   
+            new_follower.save()
+            return redirect('/profile/' + user)
+    else :
+        return redirect('/')
+
 @login_required(login_url='signin')    
 def settings (request) :
     user_profile = Profile.objects.get(user=request.user)
@@ -24,6 +43,20 @@ def settings (request) :
         user_profile.save()       
         return redirect('settings') 
     return render(request,'settings.html',{'user_profile' : user_profile})
+
+@login_required(login_url='signin')
+def upload(request): 
+    if request.method == "POST" : 
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST.get('caption')
+        if not image or not caption:
+            messages.error(request, "Please provide both an image and a caption.")
+            return 
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    return redirect('/')
 
 def signup(request):
     if request.method == 'POST':
@@ -68,7 +101,41 @@ def signin (request) :
     else :
         return render(request,'signin.html')    
 
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    post = Post.objects.get(id=post_id)
+    like_filter = LikePost.objects.filter(post_id=post_id,username=username).first()
+    if not like_filter:
+        new_like = LikePost.objects.create(post_id=post_id,username=username)
+        post.no_of_likes += 1
+        post.save()
+        new_like.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes -= 1
+        post.save()
+        return redirect('/')
 
+@login_required(login_url='signin')
+def profile(request,pk) :
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user = pk)
+    user_no_of_posts = len(user_posts)
+    is_following = False
+    if FollowersCount.objects.filter(follower=request.user.username, user=pk).first():
+        is_following = True
+    context = {
+        'user_object' : user_object,
+        'user_profile' : user_profile,
+        'user_posts' : user_posts,
+        'user_no_of_posts' : user_no_of_posts,
+        'is_following' : is_following,
+    }
+    return render(request,'profile.html',context)    
 
 @login_required(login_url='signin')    
 def logout (request) :
